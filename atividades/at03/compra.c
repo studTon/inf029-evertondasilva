1c16d439f6db1f61a0cf5bab76ba6d7648615531
studTon <bra.17.ess@gmail.com>
2021-03-10 20:30:12 -0300

    Continuei a melhorar o modulo de estoque Comecei a adicionar a funcionalidade de adicionar produto no modulo compra.

--- atividades/at03/compra.c
+++ atividades/at03/compra.c
@@ -1,4 +1,11 @@
+#include <stdio.h>
+#include "estoque.h"
+
 void adicionar_produto(int codigo, int quantidade)
 {
+    int input_codigo;
+    printf("==========================\n\tCompra\n==========================\n");
+    printf("Digite o codigo do produto: ");
+    scanf("%d", &input_codigo);
     
 }
--- atividades/at03/estoque.c
+++ atividades/at03/estoque.c
@@ -1,21 +1,43 @@
 #include <stdio.h>
 
+/*Registro do struct*/
+typedef struct
+{
+    int codigo;
+    int qtde;//Quantidade do produto
+}Produto;
+
+Produto estoqueproduto[10];
+/*Fim do struct*/
+
+/*Função para encontrar o estoque*/
 void estocagem(void)
 {   
     printf("==================================\n\tEstoque\n==================================\n\n");
 
     int quantidadeProduto;
-    typedef struct
-    {
-        int codigo;
-        int qtde;//Quantidade do produto
-    } Produto;
-
-    Produto estoqueproduto[10];
 
     /*Lista de produtos em estoque*/
     estoqueproduto[0].codigo = 1010;
     estoqueproduto[0].qtde = 95;
+    estoqueproduto[1].codigo = 1011;
+    estoqueproduto[1].qtde = 63;
+    estoqueproduto[2].codigo = 2000;
+    estoqueproduto[2].qtde = 16;
+    estoqueproduto[3].codigo = 2010;
+    estoqueproduto[3].qtde = 29;
+    estoqueproduto[4].codigo = 2025;
+    estoqueproduto[4].qtde = 51;
+    estoqueproduto[5].codigo = 2080;
+    estoqueproduto[5].qtde = 42;
+    estoqueproduto[6].codigo = 3010;
+    estoqueproduto[6].qtde = 33;
+    estoqueproduto[7].codigo = 3062;
+    estoqueproduto[7].qtde = 141;
+    estoqueproduto[8].codigo = 3099;
+    estoqueproduto[8].qtde = 12;
+    estoqueproduto[9].codigo = 4125;
+    estoqueproduto[9].qtde = 78;
     /*Fim da lista*/
 
     int codigoProduto;
@@ -32,4 +54,34 @@ void estocagem(void)
         }
     }
     printf("O produto %d tem %d unidades em estoque.\n", codigoProduto, quantidadeProduto);
+}
+
+/*Checagem de produtos*/
+void checarProduto(int codigoProduto)
+{
+    /*Lista de produtos em estoque*/
+    estoqueproduto[0].codigo = 1010;
+    estoqueproduto[0].qtde = 95;
+    estoqueproduto[1].codigo = 1011;
+    estoqueproduto[1].qtde = 63;
+    estoqueproduto[2].codigo = 2000;
+    estoqueproduto[2].qtde = 16;
+    estoqueproduto[3].codigo = 2010;
+    estoqueproduto[3].qtde = 29;
+    estoqueproduto[4].codigo = 2025;
+    estoqueproduto[4].qtde = 51;
+    estoqueproduto[5].codigo = 2080;
+    estoqueproduto[5].qtde = 42;
+    estoqueproduto[6].codigo = 3010;
+    estoqueproduto[6].qtde = 33;
+    estoqueproduto[7].codigo = 3062;
+    estoqueproduto[7].qtde = 141;
+    estoqueproduto[8].codigo = 3099;
+    estoqueproduto[8].qtde = 12;
+    estoqueproduto[9].codigo = 4125;
+    estoqueproduto[9].qtde = 78;
+    /*Fim da lista*/
+
+    printf("==================================\n\tChecagem de produto\n==================================\n\n");
+    
 }
