1120860be21e3fa7d18ec0a272d667c9012b73f1
studTon <bra.17.ess@gmail.com>
2021-03-20 16:49:54 -0300

    Finalizei a questão 8 da lista de funções

---
+++ atividades/at04/Q8.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM_STRING 64
+#define TAM_DATA 11 //Corresponde a : "01/01/0001\0"
+#define TAM_CPF 15 //Corresponde a : "123.456.789-00\0"
+void cadastrarCliente(void);
+/*Struct dos dados do cliente*/
+typedef struct
+{
+    char nome[TAM_STRING];
+    char cpf[TAM_CPF];
+    char sexo;
+    char dataNasc[TAM_DATA];
+}cliente;
+
+cliente perfilCliente;
+/*Principal*/
+int main(void)
+{
+    cadastrarCliente();
+    /*Retorno dos dados cadastrados*/
+    printf("\n===Dados cadastrados===\n\n");
+    printf("Nome: %s\n", perfilCliente.nome);
+    fflush(stdout);
+    printf("CPF: %s\n", perfilCliente.cpf);
+    printf("Sexo: %c\n", perfilCliente.sexo);
+    printf("Data de nasc.: %s\n", perfilCliente.dataNasc);
+
+    return 0;
+}
+
+void cadastrarCliente(void)
+{
+    printf("Cadastro de cliente\n");
+    //Nome
+    printf("Nome: ");
+    gets(perfilCliente.nome);
+    //CPF
+    printf("CPF(XXX.XXX.XXX-XX): ");
+    gets(perfilCliente.cpf);
+    //Sexo
+    printf("Sexo: ");
+    scanf("%c", &perfilCliente.sexo);
+    //Data de nascimento
+    while(getchar() != '\n' );/*Pular uma linha*/
+    printf("Data de nascimento(dia/mes/ano): ");
+    gets(perfilCliente.dataNasc);
+    fflush(stdout);
+}
