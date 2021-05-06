e89df62f6b1c87421eea7366824d12241197c792
studTon <bra.17.ess@gmail.com>
2021-05-05 21:18:55 -0300

    Adicionei a pasta do trabalho Também adicionei a estrutura do programa principal.

---
+++ atividades/Trabalho2/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char opcao = '1';
+	while(opcao != '0')
+	{
+		printf("Digite a opção que deseja escolher")
+		scanf("%c", &opcao);
+		
+		switch(opcao)
+		{
+			case '0': printf("Sair");break;
+			case '1':{
+
+			}break;
+			case '2':{
+
+			}break;
+			case '3':{
+
+			}break;
+			case '4':{
+					
+			}break;
+		}
+	}
+	
+	
+	return 0;
+}
